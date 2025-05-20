81b05ef255f5710ca2b59abf82e65b14acfa5c03
vinivin153 <vinivin153@naver.com>
2025-05-20 17:02:03 +0900

    feat: router 설정

---
+++ src/router.tsx
@@ -0,0 +1,9 @@
+import Home from 'pages/Home';
+import { createBrowserRouter } from 'react-router';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    Component: Home,
+  },
+]);
