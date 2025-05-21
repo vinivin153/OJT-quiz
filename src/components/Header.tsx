5a7b5187723e909a11a06aad0b016fa1a82b55d5
vinivin153 <vinivin153@naver.com>
2025-05-21 16:57:16 +0900

    feat: 퀴즈 페이지에 들어갈 Header 구현

---
+++ src/components/Header.tsx
@@ -0,0 +1,32 @@
+import { useNavigate } from 'react-router';
+
+type HeaderProps = {
+  title: string;
+};
+
+function Header({ title }: HeaderProps) {
+  const navigate = useNavigate();
+
+  /** 사용자에게 나가기 확인을 받고, 확인 시 홈 페이지로 이동하는 함수 */
+  const handleExitClick = () => {
+    const isReallyExit = confirm('정말 나가시겠습니까?');
+
+    if (isReallyExit) navigate('/');
+  };
+
+  return (
+    <header className="flex items-center justify-between w-full h-20 bg-[#5773b5]">
+      <div className="flex align-center justify-center w-40">남은시간(TODO)</div>
+      <div className="flex grow-1 items-centers justify-center">
+        <h1 className="text-3xl text-white" style={{ fontFamily: 'BMJUA' }}>
+          {title}
+        </h1>
+      </div>
+      <div className="flex w-40 align-center justify-center">
+        <button onClick={handleExitClick}>나가기</button>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
