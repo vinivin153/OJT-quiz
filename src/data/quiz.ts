ec1688c0f2c85887a51d7ca668d738a74e60463b
vinivin153 <vinivin153@naver.com>
2025-05-21 16:56:17 +0900

    feat: 문제 데이터 생성

---
+++ src/data/quiz.ts
@@ -0,0 +1,53 @@
+export const IDOL_QUIZ_LIST = [
+  {
+    id: 1,
+    front: ['장원영', '나띠', '제니', '김나경'],
+    back: ['아이브', '키스오브라이프', '블랙핑크', '트리플에스'],
+  },
+  {
+    id: 2,
+    front: ['김채원', '설윤', '채령', '이채영'],
+    back: ['르세라핌', '엔믹스', '있지', '프로미스나인'],
+  },
+  {
+    id: 3,
+    front: ['카리나', '츠키', '민주', '진'],
+    back: ['에스파', '빌리', '아일릿', 'BTS'],
+  },
+];
+
+export const MATH_QUIZ_LIST = [
+  {
+    id: 1,
+    question: ['2 + 2 = '],
+    answer: ['4'],
+  },
+  {
+    id: 2,
+    question: ['43 - 19 = '],
+    answer: ['24'],
+  },
+  {
+    id: 3,
+    question: ['13 * 3 = '],
+    answer: ['39'],
+  },
+];
+
+export const ENG_QUIZ_LIST = [
+  {
+    id: 1,
+    parts: ['i', 'have', 'a pen'],
+    answer: 'i have a pen',
+  },
+  {
+    id: 2,
+    parts: ['she', 'is', 'my', 'friend'],
+    answer: 'she is my friend',
+  },
+  {
+    id: 3,
+    parts: ['the cat', 'is', 'under', 'the table'],
+    answer: 'the cat is under the table',
+  },
+];
