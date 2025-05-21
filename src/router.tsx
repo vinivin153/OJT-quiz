d29b3311b6f6ac6fef4ee048dc099a9d850b75b9
vinivin153 <vinivin153@naver.com>
2025-05-21 17:01:39 +0900

    feat: 퀴즈 페이지 router 설정

--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,30 @@
 import Home from 'pages/Home';
+import MathQuiz from 'pages/MathQuiz';
+import QuizLayout from 'pages/QuizLayout';
 import { createBrowserRouter } from 'react-router';
 
+// TODO: 각 퀴즈 페이지 구현 후 연결하기
 export const router = createBrowserRouter([
   {
     path: '/',
-    Component: Home,
+    element: <Home />,
+  },
+  {
+    path: '/quiz',
+    element: <QuizLayout />,
+    children: [
+      {
+        path: 'math',
+        element: <MathQuiz />,
+      },
+      {
+        path: 'english',
+        element: <MathQuiz />,
+      },
+      {
+        path: 'idol',
+        element: <MathQuiz />,
+      },
+    ],
   },
 ]);
