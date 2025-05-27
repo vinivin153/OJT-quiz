0355bd679ec238984b8a9bd4c81bd532d40371f2
vinivin153 <vinivin153@naver.com>
2025-05-27 10:24:31 +0900

    feat: 아이돌 퀴즈 데이터 타입 변경

--- src/data/quiz.ts
+++ src/data/quiz.ts
@@ -1,21 +1,3 @@
-export const IDOL_QUIZ_LIST = [
-  {
-    id: 1,
-    front: ['장원영', '나띠', '제니', '김나경'],
-    back: ['아이브', '키스오브라이프', '블랙핑크', '트리플에스'],
-  },
-  {
-    id: 2,
-    front: ['김채원', '설윤', '채령', '이채영'],
-    back: ['르세라핌', '엔믹스', '있지', '프로미스나인'],
-  },
-  {
-    id: 3,
-    front: ['카리나', '츠키', '민주', '진'],
-    back: ['에스파', '빌리', '아일릿', 'BTS'],
-  },
-];
-
 export const MATH_QUIZ_LIST = [
   {
     id: 1,
@@ -37,17 +19,60 @@ export const MATH_QUIZ_LIST = [
 export const ENG_QUIZ_LIST = [
   {
     id: 1,
-    parts: ['i', 'have', 'a pen'],
+    parts: ['have', 'i', 'a pen'],
     answer: 'i have a pen',
   },
   {
     id: 2,
-    parts: ['she', 'is', 'my', 'friend'],
+    parts: ['friend', 'she', 'my', 'is'],
     answer: 'she is my friend',
   },
   {
     id: 3,
-    parts: ['the cat', 'is', 'under', 'the table'],
-    answer: 'the cat is under the table',
+    parts: ['eat', 'i', 'chicken', 'want to'],
+    answer: 'i want to eat chicken',
+  },
+];
+
+type IdolQuiz = {
+  id: number;
+  front: string[];
+  back: string[];
+  answer: Record<string, string>;
+};
+
+export const IDOL_QUIZ_LIST: IdolQuiz[] = [
+  {
+    id: 1,
+    front: ['장원영', '제니', '김나경', '나띠'],
+    back: ['아이브', '키스오브라이프', '블랙핑크', '트리플에스'],
+    answer: {
+      장원영: '아이브',
+      나띠: '키스오브라이프',
+      제니: '블랙핑크',
+      김나경: '트리플에스',
+    },
+  },
+  {
+    id: 2,
+    front: ['설윤', '김채원', '이채영', '채령'],
+    back: ['르세라핌', '엔믹스', '있지', '프로미스나인'],
+    answer: {
+      김채원: '르세라핌',
+      설윤: '엔믹스',
+      채령: '있지',
+      이채영: '프로미스나인',
+    },
+  },
+  {
+    id: 3,
+    front: ['츠키', '민주', '진', '카리나'],
+    back: ['에스파', '빌리', '아일릿', 'BTS'],
+    answer: {
+      카리나: '에스파',
+      츠키: '빌리',
+      민주: '아일릿',
+      진: 'BTS',
+    },
   },
 ];
