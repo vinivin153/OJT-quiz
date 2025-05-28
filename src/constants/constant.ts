85eb99607692afb9d84a22902a29961a294c448d
vinivin153 <vinivin153@naver.com>
2025-05-28 14:13:08 +0900

    feat: 헤더 title 상수 추가

--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -1,3 +1,9 @@
 export const TOTAL_STEPS = 3;
+export const TOTAL_CHANCES = 3;
+
 export const SUBJECTS = ['수학', '영어', '아이돌'] as const;
+export const MATH_TITLE = '1교시 - 수학';
+export const ENGLISH_TITLE = '2교시 - 영어';
+export const IDOL_TITLE = '3교시 - 아이돌';
+
 export const INIT_ANSWER = '';
