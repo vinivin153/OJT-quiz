503530cfa7d138927fbde7e8965ce045d7fc8463
vinivin153 <vinivin153@naver.com>
2025-05-28 15:59:16 +0900

    feat: 정답확인 로직 custom hook으로 분리

---
+++ src/hooks/useQuizHandler.tsx
@@ -0,0 +1,41 @@
+// hooks/useQuizHandler.ts
+import useHeaderStore from 'store/useHeaderStore';
+import useModalStore from 'store/useModalStore';
+import useStepStore from 'store/useStepStore';
+
+const useQuizHandler = () => {
+  const { nextStep } = useStepStore((state) => state);
+  const openModal = useModalStore((state) => state.openModal);
+  const { leftChance, decreaseLeftChance } = useHeaderStore((state) => state);
+
+  const handleCheckAnswer = (
+    getCurrentAnswer: () => any,
+    answer: any,
+    resetAnswer: () => void,
+    isAnswerCorrect?: (currentAnswer: any, answer: any) => boolean
+  ): void => {
+    const currentAnswer = getCurrentAnswer();
+    const isCorrectAnswer = isAnswerCorrect ? isAnswerCorrect(currentAnswer, answer) : currentAnswer === answer;
+
+    if (isCorrectAnswer) {
+      openModal('correct', '정답입니다!');
+      setTimeout(() => {
+        nextStep();
+      }, 2000);
+      return;
+    }
+
+    openModal('incorrect', '오답입니다!');
+    decreaseLeftChance();
+    resetAnswer();
+
+    if (leftChance === 1) {
+      openModal('gameOver', '게임 오버!');
+      return;
+    }
+  };
+
+  return { handleCheckAnswer };
+};
+
+export default useQuizHandler;
