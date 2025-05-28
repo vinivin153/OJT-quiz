04f1f34b06512a498ed5950e513a6eea449821d8
vinivin153 <vinivin153@naver.com>
2025-05-28 14:59:13 +0900

    feat: 퀴즈 정답확인 이미지 UR 상수 추가

--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -1,3 +1,7 @@
+import correctImage from 'assets/Images/correct.png';
+import inCorrectImage from 'assets/Images/incorrect.png';
+import gameOverImage from 'assets/Images/gameover.png';
+
 export const TOTAL_STEPS = 3;
 export const TOTAL_CHANCES = 3;
 
@@ -7,3 +11,8 @@ export const ENGLISH_TITLE = '2교시 - 영어';
 export const IDOL_TITLE = '3교시 - 아이돌';
 
 export const INIT_ANSWER = '';
+export const MODAL_IMAGE_URL = {
+  correct: correctImage,
+  incorrect: inCorrectImage,
+  gameOver: gameOverImage,
+} as const;
