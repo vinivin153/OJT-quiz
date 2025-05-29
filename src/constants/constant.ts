ab614d4d8a1860084db60bcc4f362e5f24a7fe76
vinivin153 <vinivin153@naver.com>
2025-05-29 10:44:44 +0900

    feat: canvas color 상수 추가

--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -18,3 +18,20 @@ export const MODAL_IMAGE_URL = {
   gameOver: gameOverImage,
   gameClear: gameClearImage,
 } as const;
+
+export const COLORS = {
+  background: '#f9f9fb',
+  border: '#94a3b8', // slate-400
+  frontFill: '#fef9ff', // subtle pastel pink
+  backFill: '#f0f9ff', // subtle pastel blue
+  selectedFrontFill: '#fbcfe8', // rose-200
+  selectedBackFill: '#bae6fd', // sky-200
+  text: '#1e293b', // slate-800
+  line: '#3b82f6', // blue-500
+  shadow: {
+    color: 'rgba(0, 0, 0, 0.15)',
+    offsetX: 3,
+    offsetY: 3,
+    blur: 5,
+  },
+};
